3d8ba56fab59728357164e802d5c21b53e161d16
SHMUEL_LPTP <shmuelpro@gmail.com>
2020-07-26 11:48:21 +0300

    docs: ✏️ Added useInterval for example

--- example/src/helpers.js
+++ example/src/helpers.js
@@ -1,14 +1,36 @@
-import {  useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 export function usePrevious(value) {
     // The ref object is a generic container whose current property is mutable ...
     // ... and can hold any value, similar to an instance property on a class
     const ref = useRef();
-    
+
     // Store current value in ref
     useEffect(() => {
-      ref.current = value;
+        ref.current = value;
     }, [value]); // Only re-run if value changes
-    
+
     // Return previous value (happens before update in useEffect above)
     return ref.current;
-  }
+}
+
+
+
+export function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
