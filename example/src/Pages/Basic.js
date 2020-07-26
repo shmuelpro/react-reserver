74af734dc7a65751b6e248db3632cbef5a3e6668
SHMUEL_LPTP <shmuelpro@gmail.com>
2020-07-26 11:52:55 +0300

    feat: 🎸 Added a drag and let go on basic example

--- example/src/Pages/Basic.js
+++ example/src/Pages/Basic.js
@@ -4,22 +4,45 @@ import Reserver, { Bar, useReserver, reserverReducer, createBar, getPosition, re
 
 export default function Basic(props) {
 
-    const { bars, addBar } = useReserver(reserverReducer, [])
+    const { bars, addBar, setBars } = useReserver(reserverReducer, [])
     console.log(bars)
     return <Reserver
 
         mouseDownCell={(props) => {
             const newbar = createBar(props.dimension, props.cell);
             addBar(newbar)
-        }
+        }}
+
+        mouseEnterCell={(props) => {
+            const nBars = resizeBar(bars, props)
+            setBars(nBars)
+        }}
+
+        mouseUpCell={() => {
 
 
-        }
+            /* let tcontent = { ...content }
+             tcontent[`r${props.cell.row}c${props.cell.column}`] = <div style={{ background: "purple" }}>test</div>;
+             setContent(tcontent)
+ */
+            const dBars = bars.map((bar) => {
+
+                if (bar.editing) {
+                    return { ...bar, editing: false }
+                }
+                return bar;
+            })
+            setBars(dBars)
+
+
+        }}
+
+
 
     >
         {
 
-            bars.map((bar) => { return <Bar key={bar.id} {...bar} style={{ ...getPosition( bar.row, bar.column, bar.dimension) }} /> })
+            bars.map((bar) => { return <Bar key={bar.id} {...bar} style={{ ...getPosition(bar.row, bar.column, bar.dimension) }} /> })
         }
     </Reserver>
 
