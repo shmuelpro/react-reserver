9bba7402b72bbdf155647052557256639af7153c
shmuel <shmuelpro@gmail.com>
2020-07-29 17:55:56 +0300

    feat: 🎸 Added set is editing

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -4,7 +4,8 @@ const actionTypes = {
   delete: 'DELETE',
   addToEditing: 'ADD_TO_EDITING',
   doneEditing: 'DONE_EDITING',
-  setBars: 'SET_BARS'
+  setBars: 'SET_BARS',
+  setIsEditing: 'SET_IS_EDITING'
 }
 
 export default actionTypes
