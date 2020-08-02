01f89f001f9d651f6eab112644f40ff12ed58ba3
shmuel <shmuelpro@gmail.com>
2020-08-02 16:32:52 +0300

    refactor: 💡 Moved the fun or array from head to index

--- src/Head.js
+++ src/Head.js
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './style.css'
 export default function Head(props) {
-  const [columnTitleRow, setcolumnTitleRow] = useState([])
-
-  useEffect(() => {
-    if (typeof props.columnTitleRow === 'function') {
-      setcolumnTitleRow(props.columnTitleRow(props.columnCount))
-    } else if (Array.isArray(props.columnTitleRow)) {
-      setcolumnTitleRow(props.columnTitleRow)
-    }
-  }, [props.columnTitleRow, props.columnCount])
+ 
 
   return (
     <div
       role='columnheader'
-      className={columnTitleRow.length > 0 ? styles.row : styles.row_invisible}
+      className={props.columnTitleRow.length > 0 ? styles.row : styles.row_invisible}
       style={{ height: props.dimension }}
     >
       <div
         className={`${props.rowTitleWidth>0?styles.row_cell:styles.row_invisible}`}
         style={{ width: props.rowTitleWidth, height: props.dimension + 'px' }}
       />
-      {columnTitleRow.map((headitem, i) => {
+      {props.columnTitleRow.map((headitem, i) => {
         return (
           <div
             aria-colindex={i}
