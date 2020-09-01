fd7d9153f3564f0f1083a2130beaa5f670be76b2
shmuel <shmuelpro@gmail.com>
2020-09-01 16:48:54 +0300

    refactor: 💡 Removed unused actions

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -2,8 +2,6 @@ const actionTypes = {
   add: 'ADD',
   edit: 'EDIT',
   delete: 'DELETE',
-  addToEditing: 'ADD_TO_EDITING',
-  doneEditing: 'DONE_EDITING',
   setBars: 'SET_BARS',
   setIsEditing: 'SET_IS_EDITING'
 }
