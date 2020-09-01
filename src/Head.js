f809fb1e50e865b47a25c7fa82fbe6f593894694
shmuel <shmuelpro@gmail.com>
2020-09-01 12:42:49 +0300

    feat: 🎸 using cell

--- src/Head.js
+++ src/Head.js
@@ -1,33 +1,56 @@
 import React from 'react'
-import styles from './style.css'
-import RowTitle from './RowTitle'
+import Cell from './Cell'
 export default function Head(props) {
   return (
     <div
       role='columnheader'
       className={props.rowTitleClassName}
+      style={{
+        display: props.isVisible ? 'flex' : 'none',
+        height: props.dimension.height
+      }}
     >
-        dimension={{ height: props.dimension.height, width: props.rowTitleWidth }}
+      {props.dir === 'ltr' && (
+        <Cell
+          dimension={{
+            height: props.dimension.height,
+            width: props.rowTitleWidth
+          }}
+          className={props.cantonClassName}
+        >
+          {props.canton}
+        </Cell>
       )}
       {props.columnTitles.map((headitem, i) => {
         return (
-          <div
+          <Cell
             aria-colindex={i}
             key={i}
+            onMouseOver={props.onMouseOverCell}
             dimension={props.dimension}
+            column={i}
+            row={-1}
+            className={props.columnTitleClassName}
           >
             {headitem}
-          </div>
+          </Cell>
         )
       })}
-
       {props.dir === 'rtl' && (
-          dimension={{ height: props.dimension.height, width: props.rowTitleWidth }}
+        <Cell
+          dimension={{
+            height: props.dimension.height,
+            width: props.rowTitleWidth
+          }}
+          className={props.cantonClassName}
+        >
+          {props.canton}
+        </Cell>
       )}
     </div>
   )
 }
 
 Head.defaultProps = {
-  onMouseOverCell:()=>{}
+  onMouseOverCell: () => {}
 }
